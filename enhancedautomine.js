9c1a85983c448bf730b1a2e725bc7643eedd65b1
Ephenia <ephenia9@gmail.com>
2021-10-18 11:37:28 -0400

    Create enhancedautomine.js

---
+++ enhancedautomine.js
@@ -0,0 +1,231 @@
+// ==UserScript==
+// @name        [Pokeclicker] Enhanced Auto Mine
+// @namespace   Pokeclicker Scripts
+// @match       https://www.pokeclicker.com/
+// @grant       none
+// @version     1.0
+// @author      Ephenia  (Original/Credit: Ivan Lay)
+// @description Automatically mines the Underground with Bombs. eatures adjustable settings as well.
+// ==/UserScript==
+
+var mineState;
+var smallRestoreState;
+var awaitAutoMine;
+var setThreshold;
+var autoMineTimer;
+var mineColor;
+var smallRestoreColor;
+var resetInProgress;
+var busyMining;
+var autoMineSkip;
+var layersMined;
+var trainerCards = document.querySelectorAll('.trainer-card');
+
+function initAutoMine() {
+    if (mineState == "ON") {
+        autoMineTimer = setInterval(function () {
+            doAutoMine();
+        }, 1000);
+    }
+    if (mineState == "OFF") {
+        mineColor = "danger"
+    } else {
+        mineColor = "success"
+    }
+    if (smallRestoreState == "OFF") {
+        smallRestoreColor = "danger"
+    } else {
+        smallRestoreColor = "success"
+    }
+    setThreshold = +setThreshold
+    resetInProgress = "NO"
+    localStorage.setItem("undergroundLayersMined", App.game.statistics.undergroundLayersMined());
+    layersMined = localStorage.getItem('undergroundLayersMined');
+
+    var minerHTML = document.createElement("div");
+    minerHTML.innerHTML = `<button id="auto-mine-start" class="col-12 col-md-3 btn btn-`+mineColor+`">Auto Mine [`+mineState+`]</button>
+<button id="small-restore-start" class="col-12 col-md-3 btn btn-`+smallRestoreColor+`">Auto Small Restore [`+smallRestoreState+`]</button>
+<div id="threshold-input" class="col-12 col-md-3 btn-secondary"><img title="Money" src="assets/images/currency/money.svg" height="25px">
+<input title="Enter in a value where Small Restores will stop being bought at." type="text" id="small-restore"></div>
+<div id="skip-input" class="col-12 col-md-3 btn-secondary">
+<input title="Auto skipping occurs if the max items in a layer is lower than the value put here." type="text" id="auto-skip"></div>`
+    document.querySelectorAll('#mineBody + div')[0].prepend(minerHTML)
+    $("#auto-mine-start").unwrap();
+    document.getElementById('small-restore').value = setThreshold.toLocaleString();
+    document.getElementById('auto-skip').value = autoMineSkip.toLocaleString();
+    addGlobalStyle('#threshold-input { display:flex;flex-direction:row;flex-wrap:wrap;align-content:center;justify-content:space-evenly;align-items:center; }');
+    addGlobalStyle('#skip-input { display:flex;flex-direction:row;flex-wrap:wrap;align-content:center;justify-content:space-evenly;align-items:center; }');
+    addGlobalStyle('#small-restore { width:150px; }');
+    addGlobalStyle('#auto-skip { width:150px; }');
+
+    $("#auto-mine-start").click (startAutoMine);
+    $("#small-restore-start").click (autoRestore);
+
+    function startAutoMine() {
+        if (mineState == "OFF") {
+            localStorage.setItem("autoMineState", "ON");
+            mineState = "ON"
+            autoMineTimer = setInterval(function () {
+                doAutoMine();
+            }, 1000); // Happens every 1 second
+            document.getElementById('auto-mine-start').innerText = `Auto Mine [`+mineState+`]`
+            document.getElementById("auto-mine-start").classList.remove('btn-danger');
+            document.getElementById("auto-mine-start").classList.add('btn-success');
+        } else {
+            endAutoMine();
+        }
+    }
+
+    function doAutoMine() {
+        var getEnergy = App.game.underground.energy;
+        var getMoney = App.game.wallet.currencies[GameConstants.Currency.money]();
+        var buriedItems = Mine.itemsBuried();
+        var skipsRemain = Mine.skipsRemaining();
+        var getRestores = document.getElementsByClassName('restore-item')[0].previousElementSibling.innerText.replace(/[(]|[)]/g, "")
+        var getCost = document.querySelectorAll('div[data-bind*="ShopHandler.shopObservable().items"')[1].querySelectorAll('div.col-6')[9].querySelector('[data-bind*="$data.reduced"').innerText.replace(/,/g, "");
+        var shopWindow = document.getElementById('shopModal')
+        if (buriedItems >= autoMineSkip || skipsRemain == 0) {
+            if (smallRestoreState == "ON") {
+                if ((window.getComputedStyle(shopWindow).display === "none") && (+getCost == 30000) && (+getRestores == 0) && (getMoney >= setThreshold + 30000)) {
+                    ShopHandler.showShop(pokeMartShop);
+                    //console.log(ShopHandler.shopObservable().items)
+                    ShopHandler.setSelected(9)
+                    ShopHandler.buyItem()
+                } else {
+                    //console.log("Not attempting to buy.")
+                }
+            }
+            if (getEnergy < 10) {
+                ItemList.SmallRestore.use()
+            }
+            if (getEnergy >= 10) {
+                Mine.bomb();
+            }
+        } else {
+            if (resetInProgress == "NO") {
+                if (Mine.itemsBuried() >= Mine.itemsFound()) {
+                    // Don't resolve queued up calls to checkCompleted() until completed() is finished and sets loadingNewLayer to false
+                    if (Mine.loadingNewLayer == true) {
+                        resetInProgress = "YES"
+                    }
+                    if (resetInProgress == "NO") {
+                        Mine.loadingNewLayer = true;
+                        setTimeout(Mine.completed, 1500);
+                        //GameHelper.incrementObservable(App.game.statistics.undergroundLayersMined);
+                        if (Mine.skipsRemaining() != 0) {
+                            GameHelper.incrementObservable(Mine.skipsRemaining, -1);
+                        }
+                        busyMining = setTimeout(function(){
+                            resetInProgress = "NO"
+                        } , 1500);
+                    } else {
+                        resetInProgress = "NO"
+                    }
+                }
+            }
+        }
+        if (layersMined != App.game.statistics.undergroundLayersMined()) {
+            var treasureLength = player.mineInventory().length;
+            for (var i = 0; i < treasureLength; i++) {
+                var valueType = player.mineInventory()[i].valueType
+                if (valueType == "Diamond") {
+                    var treasureID = player.mineInventory()[i].id;
+                    Underground.sellMineItem(treasureID, Infinity)
+                }
+            }
+            localStorage.setItem("undergroundLayersMined", App.game.statistics.undergroundLayersMined());
+            layersMined = localStorage.getItem('undergroundLayersMined');
+        }
+    }
+
+
+    document.querySelector('#small-restore').addEventListener('input', event => {
+        setThreshold = +event.target.value.replace(/[A-Za-z!@#$%^&*()]/g, '').replace(/[,]/g, "");
+        localStorage.setItem("autoBuyThreshold", setThreshold);
+        event.target.value = setThreshold.toLocaleString();
+    });
+
+    document.querySelector('#auto-skip').addEventListener('input', event => {
+        autoMineSkip = +event.target.value.replace(/[A-Za-z!@#$%^&*()]/g, '').replace(/[,]/g, "");
+        localStorage.setItem("autoMineSkip", autoMineSkip);
+        event.target.value = autoMineSkip.toLocaleString();
+    });
+
+    function autoRestore() {
+        if (smallRestoreState == "OFF") {
+            localStorage.setItem("autoSmallRestore", "ON");
+            smallRestoreState = "ON"
+            document.getElementById('small-restore-start').innerText = `Auto Small Restore [`+smallRestoreState+`]`
+            document.getElementById("small-restore-start").classList.remove('btn-danger');
+            document.getElementById("small-restore-start").classList.add('btn-success');
+        } else {
+            localStorage.setItem("autoSmallRestore", "OFF");
+            smallRestoreState = "OFF"
+            document.getElementById('small-restore-start').innerText = `Auto Small Restore [`+smallRestoreState+`]`
+            document.getElementById("small-restore-start").classList.remove('btn-success');
+            document.getElementById("small-restore-start").classList.add('btn-danger');
+        }
+    }
+
+    function endAutoMine() {
+        localStorage.setItem("autoMineState", "OFF");
+        mineState = "OFF"
+        document.getElementById('auto-mine-start').innerText = `Auto Mine [`+mineState+`]`
+        document.getElementById("auto-mine-start").classList.remove('btn-success');
+        document.getElementById("auto-mine-start").classList.add('btn-danger');
+        clearTimeout(busyMining);
+        //clearTimeout(mineComplete);
+        resetInProgress = "NO";
+        clearInterval(autoMineTimer)
+    }
+
+}
+
+if (localStorage.getItem('autoMineState') == null) {
+    localStorage.setItem("autoMineState", "OFF");
+}
+if (localStorage.getItem('autoSmallRestore') == null) {
+    localStorage.setItem("autoSmallRestore", "OFF");
+}
+if (localStorage.getItem('autoBuyThreshold') == null) {
+    localStorage.setItem("autoBuyThreshold", "0");
+}
+if (localStorage.getItem('autoMineSkip') == null) {
+    localStorage.setItem("autoMineSkip", "0");
+}
+mineState = localStorage.getItem('autoMineState');
+smallRestoreState = localStorage.getItem('autoSmallRestore');
+setThreshold = localStorage.getItem('autoBuyThreshold');
+autoMineSkip = localStorage.getItem('autoMineSkip');
+
+for (var i = 0; i < trainerCards.length; i++) {
+    trainerCards[i].addEventListener('click', checkAutoMine, false);
+}
+
+function checkAutoMine() {
+    awaitAutoMine = setInterval(function () {
+        var undergroundAccess = App.game.underground.canAccess();
+        if (typeof undergroundAccess === 'undefined') {
+            console.log("Farm access isn't determined yet.");
+        } else {
+            if (undergroundAccess == true) {
+                ShopHandler.showShop(pokeMartShop);
+                initAutoMine();
+                clearInterval(awaitAutoMine)
+            }
+            if (undergroundAccess == false) {
+                clearInterval(awaitAutoMine)
+            }
+        }
+    }, 1000);
+}
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
