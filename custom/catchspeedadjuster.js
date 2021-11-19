7d5eb590c61a8f30d68e91dd67a919c53c000898
Ephenia <ephenia9@gmail.com>
2021-11-19 00:03:27 -0500

    Create catchspeedadjuster.js

---
+++ custom/catchspeedadjuster.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name        [Pokeclicker] Catch Speed Adjuster
+// @namespace   Pokeclicker Scripts
+// @match       https://www.pokeclicker.com/
+// @grant       none
+// @version     1.0
+// @author      Ephenia
+// @description Adjusts catch speed of all Pokeballs. Currently only makes Pokeballs catch as fast as possible.
+// ==/UserScript==
+
+var ballAdjuster;
+var getBalls;
+var awaitBallAdjust;
+var defaultTime = [];
+var trainerCards = document.querySelectorAll('.trainer-card');
+
+function initBallAdjust() {
+    var getBalls =  App.game.pokeballs.pokeballs;
+    for (var i = 0; i < getBalls.length; i++) {
+        defaultTime.push(getBalls[i].catchTime)
+    }
+    var ballCont = document.getElementById('pokeballSelectorBody').querySelector('thead');
+    var ballAdj = document.createElement("tr");
+    ballAdj.innerHTML = `<td colspan="4"><div style="height: 25px;"><label for="ball-adjust">0 Delay Capture <label><input id="ball-adjust" type="checkbox" style="position: relative;top: 2px;"></div></td>`
+    ballCont.append(ballAdj)
+    document.getElementById('ball-adjust').addEventListener('click', event => changeAdjust(event.target));
+
+    if (ballAdjuster == "true") {
+        document.getElementById('ball-adjust').checked = true;
+        catchDelay();
+    }
+
+    function changeAdjust(ele) {
+        if (ballAdjuster == "true") {
+            ballAdjuster = "false"
+        } else {
+            ballAdjuster = "true"
+        }
+        localStorage.setItem("ballAdjuster", ballAdjuster);
+        catchDelay();
+    }
+
+    function catchDelay() {
+        for (var i = 0; i < getBalls.length; i++) {
+            if (ballAdjuster == "true") {
+                getBalls[i].catchTime = 0;
+            } else {
+                getBalls[i].catchTime = defaultTime[i];
+            }
+        }
+    }
+}
+
+if (localStorage.getItem('ballAdjuster') == null) {
+    localStorage.setItem("ballAdjuster", "false");
+}
+ballAdjuster = localStorage.getItem('ballAdjuster');
+
+for (var i = 0; i < trainerCards.length; i++) {
+    trainerCards[i].addEventListener('click', checkBallAdjust, false);
+}
+
+function checkBallAdjust() {
+    awaitBallAdjust = setInterval(function () {
+        var gameState = App.game.gameState;
+        if (typeof gameState === 'undefined') {
+            console.log("Ball adjust isn't ready yet.");
+        } else {
+            initBallAdjust();
+            clearInterval(awaitBallAdjust)
+        }
+    }, 1000);
+}
