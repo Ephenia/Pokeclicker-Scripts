d431912a2bb5c55658982e17a559acb363997f56
Alessandro Di Martino <alessandrodimartino3@gmail.com>
2022-05-07 11:59:08 +0200

    Changed event order
    Made the events load independent of order and changed local storage to load them properly. This will reset the active events for this version because the names of the variables have changed. Additionally future proofed the script by adding events even if we don't have the images yet.

--- custom/infiniteseasonalevents.js
+++ custom/infiniteseasonalevents.js
@@ -32,16 +32,20 @@ function initEvents() {
 
     //Testing loading events in init
     for (var i = 0; i < getEvents.length; i++) {
-        if (localStorage.getItem('specialEvent'+i) == null) {
-            localStorage.setItem('specialEvent'+i, 0);
+        if (localStorage.getItem(getEvents[i].title) == null) {
+            localStorage.setItem(getEvents[i].title, 0);
         }
-        storedEvents.push(+localStorage.getItem('specialEvent'+i))
+        /*
+        Changed the storedEvents list to contain objects that have the name of the event and active status so we can search for them
+        even with a different order
+        */
+        storedEvents.push({name: getEvents[i].title, active: localStorage.getItem(getEvents[i].title)})
     }
 
     for (var ii = 0; ii < getEvents.length; ii++) {
         getEvents[ii].startTime = startDate
         getEvents[ii].endTime = endDate
-        if (getEvents[ii].hasStarted() == false && storedEvents[ii] == 1) {
+        if (getEvents[ii].hasStarted() == false && storedEvents[ii].active == 1) {
             getEvents[ii].start()
         }
     }
@@ -67,7 +71,7 @@ function initEvents() {
     eventMod.setAttribute("aria-labelledby", "eventModal");
     eventMod.setAttribute("aria-modal", "true");
     eventMod.setAttribute("role", "dialog");
-    //Event order changed, up to date as of 28/04/22
+
     eventMod.innerHTML = `<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
         <div class="modal-header" style="justify-content: space-around;">
@@ -77,56 +81,91 @@ function initEvents() {
             </button>
         </div>
         <div class="modal-body">
-        <div id="event-1" class="event-select" data-value="0"><b>`+getEvents[0].title+`</b><br>`+getEvents[0].description+`<br>
-        <img src="assets/images/pokemon/175.2.png">
-        </div><hr>
-        <div id="event-2" class="event-select" data-value="1"><b>`+getEvents[1].title+`</b><br>`+getEvents[1].description+`<br>
-        <img src="assets/images/pokemon/1.3.png">
-        </div><hr>
-        <div id="event-3" class="event-select" data-value="2"><b>`+getEvents[2].title+`</b><br>`+getEvents[2].description+`<br>
-        <img src="assets/images/pokemon/25.08.png">
-        </div><hr>
-        <div id="event-4" class="event-select" data-value="3"><b>`+getEvents[3].title+`</b><br>`+getEvents[3].description+`<br>
-        <img src="assets/images/pokemon/150.1.png">
-        <img src="assets/images/pokemon/1.1.png">
-        <img src="assets/images/pokemon/4.1.png">
-        <img src="assets/images/pokemon/7.1.png">
-        </div><hr>
-        <div id="event-5" class="event-select" data-value="4"><b>`+getEvents[4].title+`</b><br>`+getEvents[4].description+`<br>
-        <img src="assets/images/pokemon/1.2.png">
-        <img src="assets/images/pokemon/175.1.png">
-        <img src="assets/images/pokemon/25.1.png"><br>
-        <img src="assets/images/pokemon/92.png">
-        <img src="assets/images/pokemon/200.png">
-        <img src="assets/images/pokemon/353.png">
-        <img src="assets/images/pokemon/355.png">
-        </div><hr>
-        <div id="event-6" class="event-select" data-value="5"><b>`+getEvents[5].title+`</b><br>`+getEvents[5].description+`<br>
-        <img src="assets/images/pokemon/133.1.png">
-        <img src="assets/images/pokemon/25.11.png">
-        </div><hr>
-        <div id="event-7" class="event-select" data-value="6"><b>`+getEvents[6].title+`</b><br>`+getEvents[6].description+`<br>
-        <img src="assets/images/pokemon/143.1.png">
-        </div><hr>
-        <div id="event-8" class="event-select" data-value="7"><b>`+getEvents[7].title+`</b><br>`+getEvents[7].description+`<br>
-        <img src="assets/images/pokemon/666.19.png">
-        </div><hr>
-        <div id="event-9" class="event-select" data-value="7"><b>`+getEvents[8].title+`</b><br>`+getEvents[8].description+`<br>
-        <img src="assets/images/pokemon/743.png">
-        </div><hr>
-        <div>
-    </div>
-</div>`
+        </div>
+    </div>`
+
     profileModal.before(eventMod);
 
-    for (var add = 0; add < getEvents.length; add++) {
-        //This makes sure the script completes running even if the new event is not in the menu
-        if (document.getElementById('event-'+(add+1)) != null){
-            if (storedEvents[add] == 1) {
-                document.getElementById('event-'+(add+1)).style = "background-color: rgba(93, 226, 60, 0.5)"
-            }
-            document.getElementById('event-'+(add+1)).addEventListener('click', toggleEvent, false)
+    var modalBody = document.querySelector('[id=eventModal] div div [class=modal-body]')
+    //Event order seems to change on startup so this loads them independent of order, also loads any new events without images
+    for (i = 0; i<getEvents.length;i++){
+        switch(getEvents[i].title){
+            case "Flying Pikachu":
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br>
+                <img src="assets/images/pokemon/25.08.png">
+                </div><hr>`
+                break
+            case "Mewtwo strikes back!":
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br>
+                <img src="assets/images/pokemon/150.1.png">
+                <img src="assets/images/pokemon/1.1.png">
+                <img src="assets/images/pokemon/4.1.png">
+                <img src="assets/images/pokemon/7.1.png">
+                </div><hr>`
+                break
+            case "Halloween!":
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br>
+                <img src="assets/images/pokemon/1.2.png">
+                <img src="assets/images/pokemon/175.1.png">
+                <img src="assets/images/pokemon/25.1.png"><br>
+                <img src="assets/images/pokemon/92.png">
+                <img src="assets/images/pokemon/200.png">
+                <img src="assets/images/pokemon/353.png">
+                <img src="assets/images/pokemon/355.png">
+                </div><hr>`
+                break
+            case "Let's GO!":
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br>
+                <img src="assets/images/pokemon/133.1.png">
+                <img src="assets/images/pokemon/25.11.png">
+                </div><hr>`
+                break
+            case "Merry Christmas!":
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br>
+                <img src="assets/images/pokemon/143.1.png">
+                </div><hr>`
+                break
+            case "Lunar New Year":
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br>
+                <img src="assets/images/pokemon/666.19.png">
+                </div><hr>`
+                break
+            case "Easter":
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br>
+                <img src="assets/images/pokemon/175.2.png">
+                </div><hr>`
+                break
+            case "Golden Week":
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br>
+                <img src="assets/images/pokemon/1.3.png">
+                </div><hr>`
+                break
+            case "Ephenia's Gift":
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br>
+                <img src="assets/images/pokemon/743.png">
+                </div><hr>`
+                break
+            default:
+                modalBody.innerHTML +=
+                `<div id="event-`+i+`" class="event-select" data-value="`+i+`"><b>`+getEvents[i].title+`</b><br>`+getEvents[i].description+`<br><br>New event, Pokemon images coming soon, if not open an issue on github
+                </div><hr>`
         }
+    }
+    
+    for (var add = 0; add < getEvents.length; add++) {
+        if (storedEvents[add].active == 1) {
+            document.getElementById('event-'+(add)).style = "background-color: rgba(93, 226, 60, 0.5)"
+        }
+        document.getElementById('event-'+(add)).addEventListener('click', toggleEvent, false)
     }
 
     addGlobalStyle('.event-select { cursor: pointer; }');
@@ -135,16 +174,16 @@ function initEvents() {
 
 function toggleEvent() {
     var getVal = this.getAttribute('data-value');
-    var getEvent = +localStorage.getItem('specialEvent'+getVal)
+    var getEvent = +localStorage.getItem(storedEvents[getVal].name)
     if (getEvent == 0) {
         this.style = "background-color: rgba(93, 226, 60, 0.5)"
-        storedEvents[getVal] = 1
-        localStorage.setItem('specialEvent'+getVal, 1)
+        storedEvents[getVal].value = 1
+        localStorage.setItem(storedEvents[getVal].name, 1)
         getEvents[getVal].start()
     } else {
         this.style = ""
-        storedEvents[getVal] = 0
-        localStorage.setItem('specialEvent'+getVal, 0)
+        storedEvents[getVal].value = 0
+        localStorage.setItem(storedEvents[getVal].name, 0)
         getEvents[getVal].end()
     }
     //console.log(getVal)
