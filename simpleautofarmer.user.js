230827e8fe9f82fe4a5f008079b751b6c30a889c
Louis Clement <louis.clement@epitech.eu>
2022-07-29 19:02:02 +0200

    Feature: Option to automatically add Rich Mulch before harvesting

--- simpleautofarmer.user.js
+++ simpleautofarmer.user.js
@@ -3,8 +3,8 @@
 // @namespace   Pokeclicker Scripts
 // @match       https://www.pokeclicker.com/
 // @grant       none
-// @version     1.5
-// @author      Ephenia / Akwawa (update 1.5)
+// @version     1.6
+// @author      Ephenia / Akwawa (update 1.5) / Rocail (update 1.6)
 // @description It Add buttons to automatically plant any specific berry, harvest or mulch all berries. Make sure to have the berry selected that you want to auto plant & harvest before enabling it. This includes an auto Mulcher as well.
 // @updateURL   https://raw.githubusercontent.com/Ephenia/Pokeclicker-Scripts/master/simpleautofarmer.user.js
 // ==/UserScript==
@@ -19,6 +19,8 @@ function initAutoFarm() {
     var mulchState;
     var mulchColor;
     var autoMulchTimer;
+    var smartMulchState;
+    var smartMulchColor;
     var shovelList = document.getElementById('shovelList');
     var colorOn = "success";
     var colorOff = "danger";
@@ -26,19 +28,23 @@ function initAutoFarm() {
     plantState = localStorage.getItem('autoPlantState');
     harvestState = localStorage.getItem('autoHarvestState');
     mulchState = localStorage.getItem('autoMulchState');
+    smartMulchState = localStorage.getItem('smartMulchState');
 
     createMenu();
     autoPlant(document.getElementById('auto-plant-start'), true);
     autoHarvest(document.getElementById('auto-harvest-start'), true);
     autoMulch(document.getElementById('auto-mulch-start'), true);
+    smartMulch(document.getElementById('smart-mulch-start'), true);
 
     function createMenu() {
         plantColor = (plantState === "OFF") ? colorOff : colorOn ;
         harvestColor = (harvestState === "OFF") ? colorOff : colorOn ;
         mulchColor = (mulchState === "OFF") ? colorOff : colorOn ;
+        smartMulchColor = (smartMulchState === "OFF") ? colorOff : colorOn ;
 
         var elemAF = document.createElement("div");
         var divMenu = document.createElement("div");
+        var divMenuRow2 = document.createElement("div");
         var divBoutonAutoPlant = document.createElement("div");
         var divBoutonAutoHarvest = document.createElement("div");
         var divBoutonAutoMulch = document.createElement("div");
@@ -46,10 +52,15 @@ function initAutoFarm() {
         var buttonAutoHarvest = document.createElement("bouton");
         var buttonAutoMulch = document.createElement("bouton");
 
+        var divBoutonSmartMulch = document.createElement("div");
+        var buttonSmartMulch = document.createElement("bouton");
+
         divMenu.className = "row justify-content-center py-0";
+        divMenuRow2.className = "row justify-content-center py-0";
         divBoutonAutoPlant.className = "col-4 pr-0";
         divBoutonAutoHarvest.className = "col-4 pr-0 pl-0";
         divBoutonAutoMulch.className = "col-4 pl-0";
+        divBoutonSmartMulch.className = "col-12";
 
         buttonAutoPlant.style.fontSize = "9pt";
         buttonAutoPlant.className = "btn btn-block btn-" + plantColor;
@@ -69,14 +80,22 @@ function initAutoFarm() {
         buttonAutoMulch.textContent = " Auto Mulch [" + mulchState + "]";
         buttonAutoMulch.onclick = function() { autoMulch(this); };
 
+        buttonSmartMulch.style.fontSize = "9pt";
+        buttonSmartMulch.className = "btn btn-block btn-" + smartMulchColor;
+        buttonSmartMulch.setAttribute("id", "smart-mulch-start");
+        buttonSmartMulch.textContent = " Smart Mulch [" + smartMulchState + "]";
+        buttonSmartMulch.onclick = function() { smartMulch(this); };
+
         divBoutonAutoPlant.appendChild(buttonAutoPlant);
         divBoutonAutoHarvest.appendChild(buttonAutoHarvest);
         divBoutonAutoMulch.appendChild(buttonAutoMulch);
+        divBoutonSmartMulch.appendChild(buttonSmartMulch);
         divMenu.appendChild(divBoutonAutoPlant);
         divMenu.appendChild(divBoutonAutoHarvest);
         divMenu.appendChild(divBoutonAutoMulch);
+        divMenuRow2.appendChild(divBoutonSmartMulch);
         elemAF.appendChild(divMenu);
-
+        elemAF.appendChild(divMenuRow2)
         shovelList.before(elemAF);
     }
 
@@ -143,7 +162,31 @@ function initAutoFarm() {
     }
 
     function doHarvest() {
+        beforeHarvest()
         App.game.farming.harvestAll();
+    }
+
+    function beforeHarvest() {
+        if (smartMulchState === "ON") {
+            addRichMulch()
+        }
+    }
+
+    function addRichMulch() {
+        // Do not enter the loop if there is no mulch in the inventory
+        if (App.game.farming.mulchList[MulchType.Rich_Mulch]() === 0) {
+            return
+        }
+        // Check for every plot ready to harvest that doesn't already have mulch on them
+        App.game.farming.plotList.forEach((plot, index) => {
+            if (plot.isUnlocked &&
+                plot.berry !== BerryType.None &&
+                plot.stage() >= PlotStage.Berry &&
+                plot.mulchTimeLeft === 0
+            ) {
+                App.game.farming.addMulch(index, MulchType.Rich_Mulch, 1)
+            }
+        });
     }
 
     // Mulch - cmd, start, stop, do
@@ -178,6 +221,38 @@ function initAutoFarm() {
     function doMulch() {
         FarmController.mulchAll();
     }
+
+    function smartMulch(elt, init=false) {
+        if ((init === true && smartMulchState === "ON") || (init === false && smartMulchState == "OFF")) {
+            startSmartMulch(elt);
+        } else {
+            stopSmartMulch(elt);
+        }
+    }
+
+    function startSmartMulch(elt) {
+        localStorage.setItem("smartMulchState", "ON");
+        smartMulchState = "ON";
+        updateSmartMulchButton(elt, true)
+    }
+
+    function stopSmartMulch(elt) {
+        localStorage.setItem("smartMulchState", "OFF");
+        smartMulchState = "OFF";
+        updateSmartMulchButton(elt, false)
+        clearInterval(autoMulchTimer);
+    }
+
+    function updateSmartMulchButton(elt, activated = false) {
+        elt.innerText = "Smart Mulch [" + smartMulchState + "]";
+        if (activated) {
+            elt.classList.remove('btn-danger');
+            elt.classList.add('btn-success');
+        } else {
+            elt.classList.remove('btn-success');
+            elt.classList.add('btn-danger');
+        }
+    }
 }
 
 function loadScript() {
@@ -199,6 +274,7 @@ function initLocalStorage(param, value) {
 initLocalStorage("autoPlantState", "OFF");
 initLocalStorage("autoHarvestState", "OFF");
 initLocalStorage("autoMulchState", "OFF");
+initLocalStorage("smartMulchState", "OFF");
 
 var scriptName = 'simpleautofarmer';
 
