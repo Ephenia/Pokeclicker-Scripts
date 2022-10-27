e338f7bfebbbba56ff69c8d8ac04b0140568a74d
Ephenia <ephenia9@gmail.com>
2022-10-26 23:04:09 -0400

    Added deletion & reload delay
    Should help ensure actual deletion of localStorage keys. Delay should be enough to delete everything (hopefully).

--- scriptfixerupper.user.js
+++ scriptfixerupper.user.js
@@ -5,7 +5,7 @@
 // @description   A script solely for clearing out localStorage without saves being affected. Meant to be a user friendly solution for this and or for users who aren't as tech literate.
 // @copyright     https://github.com/Ephenia
 // @license       GPL-3.0 License
-// @version       1.3
+// @version       1.4
 
 // @homepageURL   https://github.com/Ephenia/Pokeclicker-Scripts/
 // @supportURL    https://github.com/Ephenia/Pokeclicker-Scripts/issues
@@ -23,16 +23,20 @@ function initFixerUpper() {
         for (let i = 0; i < localStorage.length; i++){
             const key = localStorage.key(i);
             if (!key.includes('save') && !key.includes('player') && !key.includes('settings')) {
-                localStorage.removeItem(key);
+                setTimeout(() => {
+                    localStorage.removeItem(key);
+                }, 25)
             }
+            setTimeout(() => {
+                location.reload();
+            }, (localStorage.length * 25) + 25)
         }
     }
     const isDesktop = navigator.userAgent.includes('desktop');
     if (!isDesktop) {
-        const warning = "Attempt to fix and reset script settings? This should clear out localStorage in relation to scripts and their dependencies, but should NOT affect any of your save data. You should back up your saves before doing so, just to be safe. Press OK to proceed!";
+        const warning = "Attempt to fix and reset script settings? This should clear out localStorage in relation to scripts and their dependencies, but should NOT affect any of your save data. You should back up your saves before doing so, just to be safe. Press OK to proceed!\r\n\r\nNote: This process may take a few seconds to complete and the page should reload when complete.";
         if (confirm(warning) == true) {
             clearLocalStorage();
-            location.reload();
         }
     } else {
         //we'll add logic for Desktop client here later for how we want to handle it
@@ -45,7 +49,6 @@ function loadScript(){
     Preload.hideSplashScreen = function(){
         var result = oldInit.apply(this, arguments)
         setTimeout(() => {
-            console.log("Delayed for 3 seconds.");
             initFixerUpper()
         }, 3000)
         return result
