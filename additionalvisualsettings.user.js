83efe3f9155c8af42dce648940c247fd72f54aba
umbralOptimatum <umbralOptimatum@users.noreply.github.com>
2023-07-09 13:38:31 -0400

    Removed escaped update-in-progress

--- additionalvisualsettings.user.js
+++ additionalvisualsettings.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name          [Pokeclicker] Additional Visual Settings
 // @namespace     Pokeclicker Scripts
-// @author        Ephenia (Credit: Optimatum)
+// @author        Ephenia
 // @description   Adds additional settings for hiding some visual things to help out with performance. Also, includes various features that help with ease of accessibility.
 // @copyright     https://github.com/Ephenia
 // @license       GPL-3.0 License
-// @version       2.5.1
+// @version       2.4.1
 
 // @homepageURL   https://github.com/Ephenia/Pokeclicker-Scripts/
 // @supportURL    https://github.com/Ephenia/Pokeclicker-Scripts/issues
